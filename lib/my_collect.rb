0c06b8194dcbf67d0e0b0a1a016da92df74f99ec
Robert Clarke <r.jason.clarke@gmail.com>
2020-11-03 00:59:26 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,11 @@
 def my_collect(array)
   i = 0
   result = []
+  while i < array.length
+    result.push(yield array[i])
+      i += 1
+  end
+  result
 end
 
 students = ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']
